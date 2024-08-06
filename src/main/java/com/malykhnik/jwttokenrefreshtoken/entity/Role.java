21007abc8fa014a1face161bcbd51bca062c51b8
Nikita <malykhnik@mail.ru>
2024-08-06 11:14:51 +0400

    commit

--- src/main/java/com/malykhnik/jwttokenrefreshtoken/entity/Role.java
+++ src/main/java/com/malykhnik/jwttokenrefreshtoken/entity/Role.java
@@ -1,4 +1,20 @@
 package com.malykhnik.jwttokenrefreshtoken.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(unique = true)
+    private String name;
 }
--- src/main/java/com/malykhnik/jwttokenrefreshtoken/entity/User.java
+++ src/main/java/com/malykhnik/jwttokenrefreshtoken/entity/User.java
@@ -1,4 +1,26 @@
 package com.malykhnik.jwttokenrefreshtoken.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    @JsonIgnore
+    private String password;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
 }
